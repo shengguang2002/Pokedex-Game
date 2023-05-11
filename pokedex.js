3f5e0eb10304e06a73fb5baebb52b4452c6a773e
HanyangYu <hyyu2002@uw.edu>
2023-05-11 10:43:11 -0700

    v1

--- pokedex.js
+++ pokedex.js
@@ -22,17 +22,60 @@
    * initiate event listener when the window is loaded
    */
   function init() {
-    console.log("start");
+    let moveButtons = qsa('#p1 .moves button');
+    for(let button of moveButtons) {
+        button.addEventListener('click', async function() {
+            console.log("clicked");
+            let move = button.querySelector('.move').textContent.replace(/\s/g, '');
+            await playMove(move);
+        });
+    }
+    id('flee-btn').addEventListener('click',  async function() {
+        await playMove("flee");
+    });
+    id('endgame').addEventListener('click', function(){
+        id('start-btn').classList.remove('hidden');
+        reset();
+    });
+    reset();
+    id('start-btn').addEventListener('click', gameView);
+    pokedexBuild();
+  }
+
+  function reset() {
     id('pokedex-view').classList.remove('hidden');
     id('p2').classList.add('hidden');
     qs('#p1 .hp-info').classList.add('hidden');
     id('results-container').classList.add('hidden');
     id('flee-btn').classList.add('hidden');
-    id('start-btn').classList.remove('hidden');
-    pokedexBuild();
-    id('start-btn').addEventListener('click', gameView);
+    id('endgame').classList.add('hidden');
+    let moveButtons = qsa('#p1 .moves button');
+    for (let button of moveButtons) {
+      button.disabled = true;
+    }
+    id('p2-turn-results').classList.add('hidden');
+    id('p1-turn-results').classList.add('hidden');
+    qs('h1').textContent = 'Your Pokedex';
   }
 
+  function endGame(playerWon, pokeName) {
+    // 显示游戏结果
+    qs('h1').textContent = playerWon ? 'You won!' : 'You lost!';
+    // 禁用所有移动按钮
+    let moveButtons = qsa('#p1 .moves button');
+    for (let button of moveButtons) {
+      button.disabled = true;
+    }
+    id('endgame').classList.remove('hidden');
+    id('flee-btn').classList.add('hidden');
+    if (playerWon && !id(pokeName).classList.contains('found')) {
+      let spriteFound = id(pokeName);
+      spriteFound.classList.add('found');
+      spriteFound.addEventListener('click', function() {
+        displayPokemonData(pokeName, '#p1');
+      });
+    }
+  }
   /**
    * Returns the element with the specified id attribute.
    * @param {string} id string representing the id attribute of the element to be returned.
@@ -44,8 +87,8 @@
 
 
   async function pokedexBuild() {
+    id("pokedex-view").innerHTML = "";
     const POKEDEX_URL = 'https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/pokedex.php?pokedex=all';
-    console.log("1");
     try {
       let response = await fetch(POKEDEX_URL);
       await statusCheck(response);
@@ -72,6 +115,7 @@
       img.addEventListener("click", function() {
         if (this.classList.contains("found")) {
           currentPokemon = pokemonShortName;
+          id('start-btn').classList.remove('hidden');
           displayPokemonData(pokemonShortName, '#p1');
         }
       });
@@ -83,39 +127,6 @@
       id("pokedex-view").append(img);
     }
   }
-
-  /*async function displayPokemonData(pokemonShortName) {
-    const API_BASE_URL = "https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/";
-    try {
-      let response = await fetch(API_BASE_URL + 'pokedex.php?pokemon=' + pokemonShortName);
-      await statusCheck(response);
-      let data = await response.json();
-      qs('#p1 .name').textContent = data.name;
-      qs('#p1 .pokepic').src = API_BASE_URL + data.images.photo;
-      qs('#p1 .type').src = API_BASE_URL + data.images.typeIcon;
-      qs('#p1 .weakness').src = API_BASE_URL + data.images.weaknessIcon;
-      qs('#p1 .hp').textContent = data.hp + 'HP';
-      qs('#p1 .info').textContent = data.info.description;
-      // Populate moves
-      let moves = document.querySelectorAll('#p1 .moves button');
-      for (let i = 0; i < moves.length; i++) {
-        if (i < data.moves.length) {
-          moves[i].querySelector('.move').textContent = data.moves[i].name;
-          moves[i].querySelector('img').src = API_BASE_URL + "icons/" + data.moves[i].type + ".jpg";
-          if (data.moves[i].dp) {
-            moves[i].querySelector('.dp').textContent = data.moves[i].dp + ' DP';
-          }
-          moves[i].classList.remove('hidden');
-        } else {
-          moves[i].classList.add('hidden');
-        }
-      }
-      // Make the start button visible
-      id('start-btn').classList.remove('hidden');
-    } catch (err) {
-      console.error(err);
-    }
-  }*/
 
   async function displayPokemonData(pokemonShortName, playerId) {
     const API_BASE_URL = "https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/";
@@ -153,15 +164,20 @@
 }
 
   async function gameView() {
-    console.log("called");
+    qs('#p1 .health-bar').classList.remove('low-health');
+    qs('#p2 .health-bar').classList.remove('low-health');
+    qs('#p1 .health-bar').style.width = `100%`;
+    qs('#p2 .health-bar').style.width = `100%`;
     id('pokedex-view').classList.add('hidden');
     id('p2').classList.remove('hidden');
     qs('#p1 .hp-info').classList.remove('hidden');
     id('results-container').classList.remove('hidden');
     id('flee-btn').classList.remove('hidden');
     id('start-btn').classList.add('hidden');
-    const moveButtons = document.querySelectorAll('#p1 .moves button');
-    moveButtons.forEach(button => button.disabled = false);
+    let moveButtons = qsa('#p1 .moves button');
+    for (let button of moveButtons) {
+      button.disabled = false;
+    }
     const GAME_URL = "https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/game.php";
     document.querySelector('h1').textContent = 'Pokemon Battle!';
     let pokeData = new FormData();
@@ -181,6 +197,53 @@
     displayPokemonData(data.p2.shortname, '#p2');
   }
 
+  async function playMove(move) {
+    const GAME_URL = "https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/game.php";
+    document.querySelector('#loading').classList.remove('hidden');
+    const formData = new FormData();
+    formData.append('guid', guid);
+    formData.append('pid', pid);
+    formData.append('movename', move);
+    try {
+        let response = await fetch(GAME_URL, {method: 'POST', body: formData});
+        await statusCheck(response);
+        let data = await response.json();
+        updateGameState(data);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        document.querySelector('#loading').classList.add('hidden');
+    }
+}
+
+function updateGameState(data) {
+  id('p1-turn-results').classList.remove('hidden');
+  id('p2-turn-results').classList.remove('hidden');
+  console.log(data);
+  let p1HP = data.p1['current-hp'];
+  qs('#p1 .card .hp').textContent = `${p1HP}HP`;
+  let p2HP = data.p2['current-hp'];
+  qs('#p2 .card .hp').textContent = `${p2HP}HP`;
+  qs('#p1 .health-bar').style.width = `${p1HP / data.p1.hp * 100}%`;
+  if(p1HP / data.p1.hp <= 0.2) {
+    qs('#p1 .health-bar').classList.add('low-health');
+  }
+  qs('#p2 .health-bar').style.width = `${p2HP / data.p2.hp * 100}%`;
+  if(p2HP / data.p2.hp <= 0.2) {
+    qs('#p2 .health-bar').classList.add('low-health');
+  }
+  id('p1-turn-results').textContent = `Player 1 played ${data.results['p1-move']} and ${data.results['p1-result']}!`;
+  if (data.results['p2-move'] && data.results['p2-result']) {
+    id('p2-turn-results').textContent = `Player 2 played ${data.results['p2-move']} and ${data.results['p2-result']}!`;
+  } else {
+    id('p2-turn-results').classList.add('hidden');
+  }
+  if (p1HP === 0) {
+    endGame(false, data.p2.shortname);
+  } else if (p2HP === 0) {
+    endGame(true, data.p2.shortname);
+  }
+}
   /**
    * Checks the response status and throws an error if it's not OK.
    * @param {Response} res - The fetch response object
